c2117f6b13de5716d61e097a5b61e83f0b8e69cc
Toshiki Sakai <tosshi18@gmail.com>
2024-08-11 13:59:30 +0900

    update: テストケース追加

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,5 +1,21 @@
 #include "MutantStack.hpp"
 #include <iostream>
+#include <list>
+
+void test_orthodox_canonical_form()
+{
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(4);
+    mstack.push(3);
+    std::cout << mstack << std::endl;
+
+    MutantStack<int> mstack2(mstack);
+    std::cout << mstack2 << std::endl;
+    MutantStack<int> mstack3;
+    mstack3 = mstack2;
+    std::cout << mstack3 << std::endl;
+}
 
 void test_subject()
 {
@@ -26,7 +42,33 @@ void test_subject()
     std::stack<int> s(mstack);
 }
 
+void test_compare_with_list()
+{
+    MutantStack<int> mstack;
+    mstack.push(1);
+    mstack.push(2);
+    mstack.push(3);
+    mstack.push(4);
+    mstack.push(5);
+    
+    std::list<int> lst;
+    lst.push_back(1);
+    lst.push_back(2);
+    lst.push_back(3);
+    lst.push_back(4);
+    lst.push_back(5);
+
+    for (MutantStack<int>::iterator mit = mstack.begin(); mit < mstack.end(); mit++) {
+        std::cout << *mit << std::endl;
+    }
+    for (std::list<int>::iterator lit = lst.begin(); lit != lst.end(); lit++) {
+        std::cout << *lit << std::endl;
+    }
+}
+
 int main()
 {
+    test_orthodox_canonical_form();
     test_subject();
+    test_compare_with_list();
 }
